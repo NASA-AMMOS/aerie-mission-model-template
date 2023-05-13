7b7f5d6fbd0ab9a120d2820ad7adc653fba7a21c
Meghana <kumar.n.meghana@gmail.com>
2023-05-12 22:23:21 -0700

    Update package-info.java to include activity annotations

--- src/main/java/firesat/package-info.java
+++ src/main/java/firesat/package-info.java
@@ -2,9 +2,17 @@
 @WithMappers(BasicValueMappers.class)
 @WithConfiguration(Configuration.class)
 @WithActivityType(GncChangeControlMode.class)
+@WithActivityType(DownlinkData.class)
+@WithActivityType(AtApoapsis.class)
+@WithActivityType(InShadow.class)
+
 package firesat;
 
+import firesat.activities.gnc.AtApoapsis;
+import firesat.activities.gnc.DownlinkData;
 import firesat.activities.gnc.GncChangeControlMode;
+import firesat.activities.gnc.InShadow;
+import firesat.models.mms.ShadowType;
 import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.MissionModel;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.MissionModel.WithActivityType;
