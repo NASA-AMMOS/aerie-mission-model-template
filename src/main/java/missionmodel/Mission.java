09c428a43e9bc3d079d1f292c747eda17f5af983
Ferguson, Eric W (397B) <Eric.W.Ferguson@jpl.nasa.gov>
2024-03-03 14:32:28 -0800

    Added example classes from modeling tutorial

--- src/main/java/missionmodel/Mission.java
+++ src/main/java/missionmodel/Mission.java
@@ -3,6 +3,8 @@ package missionmodel;
 // import gov.nasa.jpl.aerie.contrib.serialization.mappers.DoubleValueMapper;
 // import gov.nasa.jpl.aerie.contrib.streamline.core.MutableResource;
 import gov.nasa.jpl.aerie.contrib.streamline.modeling.Registrar;
+
+import static gov.nasa.jpl.aerie.merlin.framework.ModelActions.spawn;
 // import gov.nasa.jpl.aerie.contrib.streamline.modeling.discrete.Discrete;
 
 // import static gov.nasa.jpl.aerie.contrib.streamline.core.MutableResource.resource;
@@ -22,12 +24,21 @@ public final class Mission {
   // Example resource declaration
   // public MutableResource<Discrete<Double>> ExampleResource;
 
+  // Example model declaration
+  //public final DataModel dataModel;
+
   public Mission(final gov.nasa.jpl.aerie.merlin.framework.Registrar registrar, final Configuration config) {
     this.errorRegistrar = new Registrar(registrar, Registrar.ErrorBehavior.Log);
 
     // Example resource definition and registration
     // ExampleResource = resource(discrete(0.0));
     // errorRegistrar.discrete("ExampleResource", ExampleResource, new DoubleValueMapper());
+
+    // Example model instantiation
+    //this.dataModel = new DataModel(this.errorRegistrar, config);
+
+    // Example daemon task call
+    // spawn(dataModel::integrateDataRate);
 
   }
 }
