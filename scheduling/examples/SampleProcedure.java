3dec28442e33ccc526a431b659e6a115339ac3d8
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-09-05 17:04:10 -0700

    Update to work with type-utils

--- scheduling/examples/SampleProcedure.java
+++ scheduling/examples/SampleProcedure.java
@@ -1,17 +1,17 @@
 package scheduling.procedures;
 
+import gov.nasa.ammos.aerie.procedural.scheduling.Goal;
 import gov.nasa.ammos.aerie.procedural.scheduling.plan.EditablePlan;
-import gov.nasa.ammos.aerie.procedural.scheduling.Rule;
 import gov.nasa.ammos.aerie.procedural.scheduling.annotations.SchedulingProcedure;
+import gov.nasa.ammos.aerie.procedural.timeline.payloads.activities.DirectiveStart;
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
-import gov.nasa.ammos.aerie.timeline.payloads.activities.DirectiveStart;
 
 import missionmodel.Utils;
 
 import java.util.Map;
 
 @SchedulingProcedure
-public record SampleProcedure(int quantity) implements Rule {
+public record SampleProcedure(int quantity) implements Goal {
   @Override
   public void run(EditablePlan plan) {
     final var firstTime = Duration.hours(0);
@@ -20,9 +20,9 @@ public record SampleProcedure(int quantity) implements Rule {
     var currentTime = firstTime;
     for (var i = 0; i < quantity; i++) {
       plan.create(
-          Utils.getCollectDataActivityName(),
-          new DirectiveStart.Absolute(currentTime),
-          Map.of()
+        Utils.getCollectDataActivityName(),
+        new DirectiveStart.Absolute(currentTime),
+        Map.of()
       );
       currentTime = currentTime.plus(step);
     }
