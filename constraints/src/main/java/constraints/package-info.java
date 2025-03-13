4bdd780f3d1310c89b3bdaa097d918ac6e4b6b84
Eric Ferguson <eric.w.ferguson@jpl.nasa.gov>
2025-03-13 15:06:09 -0700

    Added constraint directory with examples for procedural constraints Refactored gradle build for scheduling to be identical to constraint build Added runtime junit test dependency to prevent runtime test issues

---
+++ constraints/src/main/java/constraints/package-info.java
@@ -0,0 +1,5 @@
+@WithMappers(BasicValueMappers.class)
+package constraints;
+
+import gov.nasa.ammos.aerie.procedural.scheduling.annotations.WithMappers;
+import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
